edf01ad34526c8be6c4852c7c8f5c0944fe33dda
avishekchy45 <avishekchy45@gmail.com>
2021-04-07 20:50:10 +0600

    SQL Updated

--- oca.sql
+++ oca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2021 at 09:30 PM
+-- Generation Time: Nov 17, 2020 at 05:03 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `ID` varchar(15) NOT NULL,
-  `PASS` varchar(255) NOT NULL DEFAULT '''123456'''
+  `PASS` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '123456'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -37,7 +37,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`ID`, `PASS`) VALUES
-('admin', '$2y$10$yBMcfjDjNOQn1lkp9eVBDuVcMrpMSSkAVTcPuR7INgmYE2JqoTW2i');
+('admin', 'admin'),
+('co-admin', '123456');
 
 -- --------------------------------------------------------
 
@@ -56,6 +57,9 @@ CREATE TABLE `assign` (
 --
 
 INSERT INTO `assign` (`COURSE_CODE`, `SECTION_ID`, `INS_ID`) VALUES
+('MGT 203(V3)', 'C4A', 'avishekchy54'),
+('OCA 101', 'C1A', 'avishekchy54'),
+('CSE 237', 'C4A', 'mehedihasan'),
 ('CSE 237(V3)', 'C4A', 'mehedihasan'),
 ('CSE 238(V3)', 'C4A', 'mehedihasan'),
 ('CSE 238(V3)', 'C4D', 'mehedihasan'),
@@ -84,7 +88,7 @@ CREATE TABLE `chat` (
 
 INSERT INTO `chat` (`MSG_TIME`, `CHAT_ID`, `SENDER`, `RECEIVER`, `MESSAGE`) VALUES
 ('2020-10-26 15:22:49', 'adminall28', 'admin', 'all', 'Welcome everyone...'),
-('2020-10-29 06:19:19', 'adminall53', 'admin', 'all', 'We are now hosted at https://online-class-assitance.000webhostapp.com/');
+('2020-10-29 06:19:19', 'adminall53', 'admin', 'all', 'Hello...');
 
 -- --------------------------------------------------------
 
@@ -105,6 +109,7 @@ CREATE TABLE `course` (
 INSERT INTO `course` (`COURSE_CODE`, `TITLE`, `CREDIT`) VALUES
 ('CSE 225(V3)', 'Algorithm Design And Analysis', 3),
 ('CSE 226(V3)', 'Algorithm Design And Analysis Laboratory', 1),
+('CSE 237', 'DATABASE MANAGEMENT', 3),
 ('CSE 237(V3)', 'Database Management System', 3),
 ('CSE 238(V3)', 'Database Management System Laboratory', 1.5),
 ('CSE 305(V3)', 'Software Engineering & Information System Design', 4),
@@ -129,7 +134,7 @@ CREATE TABLE `instructor` (
   `GENDER` char(7) NOT NULL,
   `LANGUAGES` varchar(255) DEFAULT NULL,
   `ABOUT` tinytext DEFAULT NULL,
-  `PASS` varchar(255) NOT NULL DEFAULT '''123456''',
+  `PASS` varchar(255) NOT NULL DEFAULT '123456',
   `PHOTO` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -138,60 +143,10 @@ CREATE TABLE `instructor` (
 --
 
 INSERT INTO `instructor` (`REGISTRATION`, `NAME`, `ID`, `EMAIL`, `PHONENUMBER`, `BIRTHDATE`, `GENDER`, `LANGUAGES`, `ABOUT`, `PASS`, `PHOTO`) VALUES
-('2020-10-16 09:51:18', 'MOHAMMED HASAN', 'mehedihasan', 'mehedihasan249@gmail.com', NULL, NULL, 'Male', NULL, 'Expert in Database Management System', '$2y$10$B81SewSfDkiyOiccFZM2g.AmT34L5KEER0esJWmNw4vXGnYZkZbVi', 'I-mehedihasan.jpg'),
-('2020-10-16 09:51:18', 'SYED MD. MINHAZ HOSSAIN', 'minhazpuccse', '', NULL, NULL, 'Male', NULL, NULL, '$2y$10$B81SewSfDkiyOiccFZM2g.AmT34L5KEER0esJWmNw4vXGnYZkZbVi', 'I-minhazpuccse.jpg');
-
---
--- Triggers `instructor`
---
-DELIMITER $$
-CREATE TRIGGER `INSPASSUPDATE` AFTER UPDATE ON `instructor` FOR EACH ROW INSERT INTO passchange
-SET
-    id = old.ID,
-    CHANGEDdate = NOW()
-$$
-DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `passchange`
---
-
-CREATE TABLE `passchange` (
-  `ID` varchar(255) NOT NULL,
-  `CHANGEDDATE` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `passchange`
---
-
-INSERT INTO `passchange` (`ID`, `CHANGEDDATE`) VALUES
-('avishekchy45', '2020-11-21 21:07:52'),
-('avishekchy45', '2020-11-21 21:08:07'),
-('avishekchy54', '2020-11-21 21:12:08'),
-('avishekchy54', '2020-11-21 21:12:41'),
-('mehedihasan', '2020-11-22 06:16:09'),
-('mehedihasan', '2020-11-22 07:13:04'),
-('mehedihasan', '2020-11-24 18:55:33'),
-('mehedihasan', '2020-11-24 18:56:37'),
-('avishekchy45', '2020-11-24 19:19:45'),
-('avishekchy45', '2020-11-24 19:20:37'),
-('new', '2020-11-24 19:29:01'),
-('new', '2020-11-24 19:30:01'),
-('new', '2020-11-24 19:30:33'),
-('new', '2020-11-24 19:31:34'),
-('new', '2020-11-24 19:32:15'),
-('new', '2020-11-24 19:32:31'),
-('new', '2020-11-24 19:32:49'),
-('new', '2020-11-24 19:33:06'),
-('new', '2020-11-24 19:34:35'),
-('avishekchy45', '2021-02-25 20:10:02'),
-('saionty45', '2021-02-25 20:11:12'),
-('meghla45', '2021-02-25 20:11:45'),
-('mehedihasan', '2021-02-25 20:12:30'),
-('minhazpuccse', '2021-02-25 20:12:37');
+('2020-10-14 16:03:07', 'Avishek Chowdhury', 'avishekchy54', 'avishekchy54@gmail.com', '01816486550', '0000-00-00', 'Male', 'Bangla, English', '', '123456', 'I-avishekchy54.jpg'),
+('2020-09-05 15:23:02', 'Avishek Chowdhury', 'I-1020', 'avishekchy54@gmail.com', '', '0000-00-00', 'Male', '', 'I am the first instructor of OCA.\r\nI am the first instructor of OCA.\r\nI am the first instructor of OCA.', 'aA1234', 'I-1020.jpg'),
+('2020-10-16 09:51:18', 'MOHAMMED HASAN', 'mehedihasan', '', NULL, NULL, 'Male', NULL, NULL, 'Aa1234', 'I-mehedihasan.jpg'),
+('2020-10-16 09:51:18', 'SYED MD. MINHAZ HOSSAIN', 'minhazpuccse', '', NULL, NULL, 'Male', NULL, NULL, 'Aa1234', 'I-minhazpuccse.jpg');
 
 -- --------------------------------------------------------
 
@@ -207,6 +162,14 @@ CREATE TABLE `post` (
   `MATERIAL` varchar(255) NOT NULL,
   `POSTED_BY` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`POSTED_TIME`, `POST_ID`, `COURSE_CODE`, `SECTION_ID`, `MATERIAL`, `POSTED_BY`) VALUES
+('2020-10-25 17:49:19', 'OCA 101C1A33', 'OCA 101', 'C1A', 'THANKS...', 'avishekchy45'),
+('2020-10-25 17:47:38', 'OCA 101C1A59', 'OCA 101', 'C1A', 'Hello Students...', 'avishekchy54');
 
 -- --------------------------------------------------------
 
@@ -224,7 +187,7 @@ CREATE TABLE `student` (
   `GENDER` char(7) NOT NULL,
   `LANGUAGES` varchar(255) DEFAULT NULL,
   `ABOUT` tinytext DEFAULT NULL,
-  `PASS` varchar(255) NOT NULL DEFAULT '''123456''',
+  `PASS` varchar(255) NOT NULL DEFAULT '123456',
   `PHOTO` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -233,20 +196,13 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`REGISTRATION`, `NAME`, `ID`, `EMAIL`, `PHONENUMBER`, `BIRTHDATE`, `GENDER`, `LANGUAGES`, `ABOUT`, `PASS`, `PHOTO`) VALUES
-('2020-10-14 11:35:34', 'Avishek Chowdhury', 'avishekchy45', 'avishekchy45@gmail.com', '', NULL, 'Male', 'Bangla, English', 'Aa1234', '$2y$10$B81SewSfDkiyOiccFZM2g.AmT34L5KEER0esJWmNw4vXGnYZkZbVi', 'S-avishekchy45.webp'),
-('2020-10-25 10:57:48', 'Meghla Singha', 'meghla45', 'meghlasingha45@gmail.com', '01827720378', '0000-00-00', 'Female', '', 'Aa1234', '$2y$10$B81SewSfDkiyOiccFZM2g.AmT34L5KEER0esJWmNw4vXGnYZkZbVi', 'S-meghla45.jpg'),
-('2020-10-25 10:51:40', 'Saionty Singha', 'saionty45', 'saiontysingha45@gmail.com', '01827720378', '0000-00-00', 'Female', 'Bangla', 'Aa1234', '$2y$10$B81SewSfDkiyOiccFZM2g.AmT34L5KEER0esJWmNw4vXGnYZkZbVi', 'S-saionty45.webp');
-
---
--- Triggers `student`
---
-DELIMITER $$
-CREATE TRIGGER `STUPASSUPDATE` AFTER UPDATE ON `student` FOR EACH ROW INSERT INTO passchange
-SET
-    id = old.ID,
-    CHANGEDdate = NOW()
-$$
-DELIMITER ;
+('2020-10-14 11:35:34', 'Avishek Chowdhury', 'avishekchy45', 'avishekchy45@gmail.com', '', '0000-00-00', 'Male', '', '', '123456', 'S-avishekchy45.webp'),
+('2020-10-14 16:18:29', 'Bill Gates', 'bill', 'bill@outlook.com', '', '0000-00-00', 'Male', 'English', '', 'Aa1234', 'S-bill.jpg'),
+('2020-10-25 10:57:48', 'Meghla Singha', 'meghla45', 'meghlasingha45@gmail.com', '01827720378', '0000-00-00', 'Female', '', '', 'Aa1234', 'S-meghla45.jpg'),
+('2020-09-05 15:31:39', 'Avishek Chowdhury', 'S-1803410201578', 'avishekchy45@gmail.com', '01816486550', '1999-11-29', 'Male', 'Bangla, English', 'I am the first LEARNER of OCA...\r\nI am the first LEARNER of OCA...\r\nI am the first LEARNER of OCA...', 'aA1234', 'S-1803410201578.webp'),
+('2020-10-25 10:51:40', 'Saionty Singha', 'saionty45', 'saiontysingha45@gmail.com', '01827720378', '0000-00-00', 'Female', 'Bangla', '', 'Aa1234', 'S-saionty45.webp'),
+('2020-10-14 16:20:24', 'Steve Jobs', 'stevejobs', 'jobs@yahoo.com', '', '0000-00-00', 'Male', 'English', '', 'Aa1234', 'S-stevejobs.jpeg'),
+('2020-10-14 16:15:28', 'Mark Zuckerberg ', 'zuck', 'zuck@fbmail.com', '', '0000-00-00', 'Male', 'English', '', 'Aa1234', '');
 
 -- --------------------------------------------------------
 
@@ -260,6 +216,23 @@ CREATE TABLE `taken` (
   `STD_ID` varchar(255) NOT NULL,
   `CSTATUS` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `taken`
+--
+
+INSERT INTO `taken` (`COURSE_CODE`, `SECTION_ID`, `STD_ID`, `CSTATUS`) VALUES
+('CSE 225(V3)', 'C4A', 'stevejobs', 'ACCEPTED'),
+('CSE 226(V3)', 'C4A', 'bill', 'pending'),
+('CSE 237', 'C4A', 'avishekchy45', 'ACCEPTED'),
+('CSE 237(V3)', 'C4A', 'bill', 'pending'),
+('CSE 237(V3)', 'C4A', 'stevejobs', 'pending'),
+('CSE 238(V3)', 'C4D', 'bill', 'pending'),
+('CSE 238(V3)', 'C4A', 'stevejobs', 'ACCEPTED'),
+('CSE 238(V3)', 'C4A', 'zuck', 'pending'),
+('CSE 305(V3)', 'C5A', 'zuck', 'pending'),
+('MGT 203(V3)', 'C4A', 'stevejobs', 'SUSPENDED'),
+('OCA 101', 'C1A', 'avishekchy45', 'ACCEPTED');
 
 --
 -- Indexes for dumped tables
